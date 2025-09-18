8396e03fe8e1018a705c3902a190dfc44be03a29
marcinwesierski-alt <marcin.wesierski@gmail.com>
2025-09-18 20:57:19 +0200

    Dodajemy pierwsze lekcja z Pythona

---
+++ 2.2.py
@@ -0,0 +1,43 @@
+liczba_1 = 12
+liczba_2 = 25
+
+print(5)
+
+print(liczba_1 + liczba_2)
+print(liczba_2 + liczba_1)
+print(liczba_1 + liczba_1 + liczba_1)
+
+suma = liczba_1 + liczba_2
+print(suma)
+
+print("2 + 2 =", 2 + 2)
+print("2 - 2 = " ,2 - 2)
+print(" 2 / 2 =" ,2 / 2)
+print("2 * 2 =", 2 * 2)
+print("2 ** 2 =", 2 ** 2) # potęgowanie
+print("2 ** (1/2) =", 2 ** (1/2))
+print("3 // 2 =", 3 // 2) # dzielenie bez reszty
+print("3 % 2 ", 3 % 2)
+
+print("sło" + "wo")
+litera_1 = "we"
+print(litera_1 * 3)
+print(litera_1 * 3)
+print(True and True)
+print(True and False)
+print(False and False)
+print(False and True)
+print(True or True)
+print(True or False)
+print(False or False)
+print(False or True)
+print(not False)
+
+print("2 < 3", 2 < 3)
+print("2 > 3", "2" > "3")
+print("12==12")
+
+
+
+
+
---
+++ 2.py
@@ -0,0 +1,17 @@
+# Typy zmiennych:
+
+liczba_int = 42 # int()
+liczba_zmiennoprzecinkowa = 5.5 # float()
+ciag_znakow = "słowo" # str(" ") , str(' ')
+prawda_falsz = True # False
+reprezentacja_niczego = None
+slownik_pusty = {} # dict()
+slownik = {"Klucz" : "Wartość_1" , "Klucz_2" : "Wartosc_2"}
+lista_pusta = [] # list()
+lista = [1, 2, 3, "wiatrak"]
+zbior_pusty = set() # zwróci nam tylko wartości unikalne
+zbior = {"jabłko", "jabłko", "ser", "ser"}
+krotka_pusta = () #tuple()
+krotka = (True, False, None) #może przechowywać różnego typu dane
+
+print(zbior, lista, int)
